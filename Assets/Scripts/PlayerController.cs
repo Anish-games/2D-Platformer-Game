6fb7881944d52747c6eca8d25cfd082afdfcbceb
MiniNija <anishkute12@gmail.com>
2025-01-12 19:35:42 +0530

    Added Tilemap system and end level game object.
    In this commit, I implemented a platform using Unity's Tilemap system and added an "end level" game object. When the player collides with this object, the game progresses to the next level.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,11 +7,17 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed;
+    public float Jump;
     private Animator animator;
     private Rigidbody2D rd2d;
-    public float Jump;
     private bool isGrounded = false;
     private Animator playerAnimator;
+    [SerializeField] private BoxCollider2D boxCol;
+
+    // Collider Variables
+    private Vector2 boxColInitSize;
+    private Vector2 boxColInitOffset;
+
     private void Awake()
     {
         Debug.Log("Player script is activated");
@@ -20,27 +26,34 @@ public class PlayerController : MonoBehaviour
         rd2d = GetComponent<Rigidbody2D>();
     }
 
+    private void Start()
+    {
+        // Fetching initial collider properties
+        boxColInitSize = new Vector2(0.4295021f, 2.054952f);
+        boxColInitOffset = new Vector2(-0.01529616f, 0.9386219f);
+        boxCol.size = boxColInitSize;
+        boxCol.offset = boxColInitOffset;
+    }
+
     private void Update()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Jump");
-        PlayMovementAnimation(horizontal , vertical);
+        PlayMovementAnimation(horizontal, vertical);
         MoveCharacter(horizontal, vertical);
 
-
-        //if (Input.GetKey(KeyCode.LeftControl))
-        //{
-        //    Crouch(true);
-        //}
-        //else
-        //{
-        //    Crouch(false);
-        //}
-
-
+        // Check for crouch input
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            Crouch(true);
+        }
+        else
+        {
+            Crouch(false);
+        }
     }
 
-    private void PlayMovementAnimation(float horizontal , float vertical)
+    private void PlayMovementAnimation(float horizontal, float vertical)
     {
         animator.SetFloat("Speed", Mathf.Abs(horizontal));
         Vector3 scale = transform.localScale;
@@ -50,58 +63,73 @@ public class PlayerController : MonoBehaviour
         }
         else if (horizontal > 0f)
         {
-            scale.x = Mathf.Abs(scale.x);       // player run animation.
+            scale.x = Mathf.Abs(scale.x); // player run animation
         }
         transform.localScale = scale;
 
-        
         if (vertical > 0)
         {
             animator.SetBool("Jump", true);
         }
-        else                                  // player jump animation.
+        else // player jump animation
         {
-            animator.SetBool("Jump",false);
+            animator.SetBool("Jump", false);
         }
-
     }
-
 
     private void MoveCharacter(float horizontal, float vertical)
     {
         Vector3 Position = transform.position;
         Position.x += horizontal * speed * Time.deltaTime;
-        transform.position = Position;                       // moving player horizontally.
+        transform.position = Position; // moving player horizontally
         if (vertical > 0 && isGrounded)
         {
             playerAnimator.SetTrigger("Jump");
-            rd2d.AddForce(new Vector2(0f,Jump),ForceMode2D.Force);
-            Debug.Log("Jumping");
+            rd2d.AddForce(new Vector2(0f, Jump), ForceMode2D.Force);
+            //Debug.Log("Jumping");
         }
-        Debug.Log("isGrounded: " + isGrounded);
+        //Debug.Log("isGrounded: " + isGrounded);
     }
+
     private void OnCollisionStay2D(Collision2D other)
     {
         if (other.transform.tag == "platform")
         {
             isGrounded = true;
+            //Debug.Log("Touching platform: " + other.transform.name);
         }
     }
-   private void OnCollisionExit2D(Collision2D other)
+
+    private void OnCollisionExit2D(Collision2D other)
     {
         if (other.transform.tag == "platform")
         {
             isGrounded = false;
+            //Debug.Log("Left platform: " + other.transform.name);
         }
     }
-   
 
+    private void Crouch(bool crouch)
+    {
+        if (crouch)
+        {
+            float offX = -0.0978f;     // Offset X
+            float offY = 0.5947f;      // Offset Y
 
+            float sizeX = 0.6988f;     // Size X
+            float sizeY = 1.3398f;     // Size Y
 
-        //public void Crouch(bool crouch)
-        //{
-        //    animator.SetBool("IsCrouch", crouch);
-        //}
+            boxCol.size = new Vector2(sizeX, sizeY);   // Setting the size of collider
+            boxCol.offset = new Vector2(offX, offY);   // Setting the offset of collider
+        }
+        else
+        {
+            // Reset collider to initial values
+            boxCol.size = boxColInitSize;
+            boxCol.offset = boxColInitOffset;
+        }
 
-
+        // Play Crouch animation
+        playerAnimator.SetBool("Crouch", crouch);
+    }
 }
